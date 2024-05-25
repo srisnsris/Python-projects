43e3a6c4b47ab0a54a610edc84435846e89d5800
srisnsris <vuppala0249@gmail.com>
2024-05-24 20:02:16 -0500

    ok

--- New/import pandas as pd.py
+++ New/import pandas as pd.py
@@ -4,7 +4,7 @@ import openpyxl
 #default first sheet
 df = pd.read_excel(r'C:\Users\srinu\OneDrive\Desktop\Book2.xlsx')
 print(df)
-print(df.columns)
+'''#print(df.columns)
 
 #loadinf particular sheet
 df_sheet_index = pd.read_excel(r'C:\Users\srinu\OneDrive\Desktop\Book2.xlsx', sheet_name='Sheet2')
@@ -15,14 +15,22 @@ print(df_sheet_multi)
 #load all sheets
 df_sheet_multi = pd.read_excel(r'C:\Users\srinu\OneDrive\Desktop\Book2.xlsx', sheet_name=None)
 print(df_sheet_multi)
-df = pd.DataFrame([[2, 'vinay'], 
-                   [3, 'Lilly'], 
-                   [4, 'Laxmi']],
-index=['one', 'two', 'three'], columns=['Claim', 'Name'])
-print(df)
+'''
+#  {'Claim': '3', 'Name': 'Alice'},{'Claim': '3', 'Name': 'Alice'}
+    # Add more rows here as needed
+df1 = pd.concat([df, pd.DataFrame([{'Claim':'2','Name':'Vinay'}])],ignore_index=True)
+print(df1)
+df1.to_excel(r'C:\Users\srinu\OneDrive\Desktop\Book2.xlsx', index=False)
+print("Modified DataFrame saved to 'Book2.xlsx'")
 
-#df.to_excel('Book2.xlsx', sheet_name='Sheet1')
-
-
-    
+#how to remove duplicates 
+import pandas as pd
+# Read the Excel file into a DataFrame
+df = pd.read_excel(r'C:\Users\srinu\OneDrive\Desktop\Book2.xlsx')
+# Remove duplicate rows based on all columns
+df.drop_duplicates(inplace=True)
+# Save the modified DataFrame back to the Excel file
+df.to_excel(r'C:\Users\srinu\OneDrive\Desktop\Book2.xlsx', index=False)
+print("Modified DataFrame saved to 'Book2.xlsx'")
+ 
 
