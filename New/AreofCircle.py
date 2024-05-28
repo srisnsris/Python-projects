a5e41f8b671edabe8c3ac63a3a7694fc9a42b40d
srisnsris <vuppala0249@gmail.com>
2024-05-27 19:09:32 -0500

    done

---
+++ New/AreofCircle.py
@@ -0,0 +1,14 @@
+
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
+
+
+def circleArea(r):
+    PI = 3.14
+    return PI * (r * r)
+
+radius = 5  # Replace with your desired radius
+area = circleArea(radius)
+print(f"The area of the circle with radius {radius} is {area:.2f}")
